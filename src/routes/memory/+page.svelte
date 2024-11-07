9fed829e50abcfebf0a5c78872b74c82c4b7086c
Evan Dunkel <43302291+evan-dunkel@users.noreply.github.com>
2024-11-07 01:23:41 -0800

    Added content to memory
    Briefcase icon for "Add Client", added "Add Employee" option

--- src/routes/memory/+page.svelte
+++ src/routes/memory/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { pageTitle } from '$lib/stores/page';
 	import FeatureCardGrid from '$lib/components/feature-card-grid.svelte';
-	import { UsersRound } from 'lucide-svelte';
+	import { UsersRound, BriefcaseBusinessIcon } from 'lucide-svelte';
 	import AiAssistantHeader from '$lib/components/ai-assistant-header.svelte';
 	$pageTitle = 'Memory';
 
@@ -17,6 +17,12 @@
 			title: 'Add Client',
 			description: 'Add a client to your organization memory.',
 			url: '/memory/add-client',
+			icon: BriefcaseBusinessIcon
+		},
+		{
+			title: 'Add Employee',
+			description: 'Add an employee to your organization memory.',
+			url: '/memory/add-employee',
 			icon: UsersRound
 		}
 	];
